0d16d937bc92048e90b8fa6e3bc05d02723ce53c
onlytherailgun <longkui111@163.com>
2017-04-19 00:45:09 +0800

    20170419

---
+++ Source/PuertoRico/Private/PR_Board_Player.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "PR_Board.h"
+#include "PR_Board_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APR_Board_Player : public APR_Board
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
